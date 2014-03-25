d598b7ecbe895d4840a45dfd47573757844451c5
dmitry <d.ryadnenko@corp.mail.ru>
2014-03-25 21:27:44 +0400

    example added

---
+++ examples/src/com/github/rongi/rotate_layout/example/MainActivity.java
@@ -0,0 +1,15 @@
+package com.github.rongi.rotate_layout.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.github.rongi.rotate_layout.example.R;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
